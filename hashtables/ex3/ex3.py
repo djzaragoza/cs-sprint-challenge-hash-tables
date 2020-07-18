7edb74971c2795b6c710a493cfb158d99a4142dd
unknown <dj.zaragoza@icloud.com>
2020-07-18 10:06:19 -0700

    working on ex3 and testing to see if it passes

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,6 +4,28 @@ def intersection(arrays):
     """
     # Your code here
 
+    # get count of array
+    arr_count = len(arrays)
+
+    # create open dict
+    total = {}
+
+    # need to find total for the first list, at 0th position
+    for i in arrays[0]:
+        # add all i to the total dict
+        total[i] = 1
+
+    # for every list after
+    for next_arr in arrays[1:]:
+        # find total occurences
+        for new_item in next_arr:
+            # if new item is in total dict
+            if new_item in total:
+                # increment total at new_item-th index by 1
+                total[new_item] += 1
+
+        # iterate through key and value in total dict and set equal to result
+        result = [key for (key, val) in total.items() if val == arr_count]
     return result
 
 
