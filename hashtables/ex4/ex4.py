8181dd4c6d3c7f74b0bedd11ac9ae5a1db55cb8c
unknown <dj.zaragoza@icloud.com>
2020-07-18 10:57:45 -0700

    debugged and re-ran test and it passed

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,21 +3,21 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-    # create open dict
-    my_dict = {}
-
-    # create open list
-    pos_nums = []
+    # create hash table
+    hash_table = {}
+    result = []
 
     # for all of the numbers in a
     for num in a:
-        # add all of the numbers to my dictionary
-        my_dict[num] = 1
+        # add all of the numbers to hash table
+        abs_val = abs(num)
 
-        if num * -1 in my_dict and num != 0:
-            pos_nums.append(abs(num))
+        if hash_table.get(abs_val) is not None:
+            result.append(abs_val)
+        else:
+            hash_table[abs_val] = abs_val
 
-    return pos_nums
+    return result
 
 
 if __name__ == "__main__":
