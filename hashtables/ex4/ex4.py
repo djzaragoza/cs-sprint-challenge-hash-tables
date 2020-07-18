2cfc00b07f42a75e8f861626a6e31820b7464f9f
unknown <dj.zaragoza@icloud.com>
2020-07-18 10:10:02 -0700

    working and testing ex4 to see if it passes test

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,21 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # create open dict
+    my_dict - {}
 
-    return result
+    # create open list
+    pos_nums = []
+
+    # for all of the numbers in a
+    for num in a:
+        # add all of the numbers to my dictionary
+        my_dict[num] = 1
+
+        if num * -1 in my_dict and num != 0:
+            pos_nums.append(abs(num))
+
+    return pos_nums
 
 
 if __name__ == "__main__":
